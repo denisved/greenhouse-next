2d13f27304ed39209f0f18998c18bf47aa531b33
denisved <denysvedernikov333@gmail.com>
2024-05-16 20:14:55 +0300

    adding darkmode

---
+++ src/components/DarkMode.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import {MdLightMode, MdDarkMode} from 'react-icons/md';
+
+import { useTheme } from 'next-themes';
+
+export default function DarkMode() {
+    const {theme, setTheme, systemTheme} = useTheme()
+    const currentTheme = theme === 'system' ? systemTheme : theme
+  return (
+    <div> 
+        {currentTheme === 'dark' ? ( <MdLightMode onClick={()=>setTheme('light')} className = 'text-3xl cursor-pointer hover:text-green-500' /> ) : (
+        <MdDarkMode onClick={()=>setTheme('dark')} className = 'text-3xl cursor-pointer hover:text-green-500' /> )} 
+    </div>
+  )
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
 import MenuItem from './MenuItem'
 import { BsFillInfoCircleFill } from 'react-icons/bs'
 import { PiPottedPlantFill } from "react-icons/pi";
 import { ImConnection } from "react-icons/im";
 import { FaDatabase } from "react-icons/fa6";
 import Link from 'next/link';
+import DarkMode from './DarkMode';
 
 export default function Header() {
   return (
     <div className='flex justify-between items-center p-5 max-w-screen-2xl mx-auto'>
-        <div className='flex gap-4'>  
+        <div className='flex gap-5'>  
             <MenuItem title ="about" address="/about" Icon={BsFillInfoCircleFill}/>
             <MenuItem title ="connect" address="/connect" Icon={ImConnection}/>
             <MenuItem title ="data storage" address="/data" Icon={FaDatabase}/>
         </div>
-        <Link href={"/"} className='flex gap-1 justify-center'>
-          <PiPottedPlantFill className='text-6xl font-bold bg-green-500 py-1 px-2 rounded-lg'/>
-        </Link>
+        <div className='flex items-center gap-10'>
+          <Link href={"/"} className='flex gap-1 justify-center'>
+            <PiPottedPlantFill className='text-6xl font-bold bg-green-500 py-1 px-1 rounded-lg'/>
+          </Link>
+          <DarkMode />
+        </div>
     </div>
   )
 }
